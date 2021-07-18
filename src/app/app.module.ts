2b71c1a0922867910a15d68d6911e6c0245692d2
unknown <sodiqofaxriddin6@gmail.com>
2021-07-19 02:06:46 +0500

    this-second

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+// import {MatCheckboxModule} from '@angular/material/checkbox';
+// import {MatButtonModule} from '@angular/material/button';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    // MatCheckboxModule
+    // MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
